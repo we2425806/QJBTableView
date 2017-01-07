dfa3f35303cef687198daed9644f186d862500da
macbookpro <macbookpro@macbookprodeMacBook-Pro-3.local>
2017-01-07 21:23:51 +0800

    创建TableView

---
+++ QJBTableView/QJBTableView/QJBTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QJBTableViewCell.swift
+//  QJBTableView
+//
+//  Created by macbookpro on 17/1/7.
+//  Copyright © 2017年 macbookpro. All rights reserved.
+//
+
+import UIKit
+
+class QJBTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- QJBTableView/QJBTableView/ViewController.swift
+++ QJBTableView/QJBTableView/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var tableView:UITableView!
+        
+        tableView = UITableView.init(frame: self.view.frame, style: UITableViewStyle.plain);
+        tableView.backgroundColor = UIColor.blue;
+        self.view.addSubview(tableView);
+        tableView.register(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cellID");
+        tableView.dataSource = self;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+   
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 10;
     }
-
+    
+    
+   
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath);
+        
+        return cell;
+    }
 
 }
 
+
+
+
+
+/// 界面属性
