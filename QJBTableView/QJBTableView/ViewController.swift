789f40b34b52d3b99e7b1310e7da3d47daac1ab6
macbookpro <macbookpro@macbookprodeMacBook-Pro-3.local>
2017-01-07 19:47:24 +0800

    项目创建

---
+++ QJBTableView/QJBTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QJBTableView
+//
+//  Created by macbookpro on 17/1/7.
+//  Copyright © 2017年 macbookpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
